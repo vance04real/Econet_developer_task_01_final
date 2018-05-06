238cbc9501b55fdbf75d59485169bd51061c6b38
Evans Chikuni <vance04real@gmail.com>
2018-05-06 10:07:22 +0200

    Fix: Dev-32:econet-utils: ERROR StatusLogger No log4j2 configuration file found in package
    added the src/main/resources folder in the module econet-utils and added the log4j2.xml file. also added the <Console> tag in the Appender tag in log4j2.xml file

--- Downloads/Compressed/developer-task-01-master/econet-utils/src/main/java/com/econetwireless/utils/enums/ResponseCode.java
+++ Downloads/Compressed/developer-task-01-master/econet-utils/src/main/java/com/econetwireless/utils/enums/ResponseCode.java
@@ -8,7 +8,7 @@ public enum ResponseCode {
     SUCCESS("200"), FAILED("500"), INVALID_REQUEST("400");
     private String code;
     private ResponseCode(String code) {
-        this.code = code;
+        this.code = code; // parameter code was assigned to itself. added the this keyword to initialize the instance variable
     }
 
     public String getCode() {
